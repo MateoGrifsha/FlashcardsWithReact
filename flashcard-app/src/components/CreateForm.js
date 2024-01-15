d3773d69df1911f481aac0b6dbb4517785df8e79
MateoGrifsha <mateogrifsha1@gmail.com>
2024-01-15 17:45:17 +0100

    the app is done, added question addition and deletion

--- flashcard-app/src/components/CreateForm.js
+++ flashcard-app/src/components/CreateForm.js
@@ -1,21 +1,62 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+ 
 const CreateForm = ()=>{
+    const [category, setCategory] = useState('maths')
+    const [question, setQuestion] = useState('')
+    const [answer, setAnswer] = useState('')
+    const [addedQuestion, setAddedQuestion] = useState(false)
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newFlashcard = {category, question, answer}
+        fetch('http://localhost:8000/cards/', {
+            method : 'POST',
+            headers : {'Content-Type' : 'application/json'},
+            body : JSON.stringify(newFlashcard)
+        }).then(()=>{
+            setAddedQuestion(true)
+        })
+
+
+
+        setTimeout(() => {
+            navigate('/'+category)
+
+        }, 1000)
+    }
+
     return(
         <div className="form-container">
-            <form>
+            <form onSubmit={handleSubmit}> 
                 <label>Category</label>
-                <select>
+                <select value={category} onChange={(e)=> {setCategory(e.target.value)}}>
                     <option value='maths'>Maths</option>
                     <option value='biology'>Biology</option>
                     <option value='physics'>Physics</option>
                 </select>
 
                 <label>Question</label>
-                <input type="text" placeholder="Question goes here..." />
+                <input 
+                type="text" 
+                placeholder="Question goes here..." 
+                value={question}
+                onChange={(e) => {setQuestion(e.target.value)}}
+                />
                 <label>Answer</label>
-                <input type="text" placeholder="Question goes here..." />
+                <input 
+                type="text" 
+                placeholder="Answer goes here..." 
+                value={answer}
+                onChange={(e) => {setAnswer(e.target.value)}}
+                />
 
-                <button>Submit</button>
+                <button>Add Question</button>
             </form>
+
+            {addedQuestion && <p className='confirmation-text'>Your question was added.</p>}
         </div>
     )
 }
--- flashcard-app/src/components/Flashcard.js
+++ flashcard-app/src/components/Flashcard.js
@@ -2,27 +2,43 @@ import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import AnswerButton from "./AnswerButton";
 import NextQuestion from "./NextQuestion";
+import { useNavigate } from 'react-router-dom'
 
 const Flashcard = (props) => {
     const { data : flashcard, error, isPending} = useFetch(`http://localhost:8000/cards/`);
-    const [flashcardCategory, setFlashcardCategory] = useState(props.category)
     const [filteredQuestions, setFilteredQuestions] = useState(Array().fill(null))
     const [filteredAnswers, setfilteredAnswers] = useState(Array().fill(null))
+    const [filteredId, setFilteredId] = useState(Array().fill(null))
     const [showQuestion, setShowQuestion] = useState(true)
     const [questionIndex, setQuestionIndex] = useState(0)
+    const [deleted, setDeleted] = useState(false)
+    const flashcardCategory = props.category
+    const navigate = useNavigate()
 
     if(flashcard){
         flashcard.filter((question) => {
             if(question.category === flashcardCategory){
                 filteredQuestions.push(question.question)
                 filteredAnswers.push(question.answer)
+                filteredId.push(question.id)
             }})
     }
 
+    const handleDelete = (flashcardId) => {
+        fetch('http://localhost:8000/cards/' + flashcardId, {
+            method : 'DELETE'
+          }).then(() => {
+            setDeleted(true)
+            setTimeout(()=>{
+                navigate("/"+flashcardCategory)
+            }, 1000)
+          })
+    }
     
     const onAnswerClick = () =>{
         setShowQuestion(!showQuestion)
     }
+
     const onNextQuestion = () => {
         setShowQuestion(!showQuestion)
         if(questionIndex === filteredQuestions.length + 1){
@@ -41,7 +57,8 @@ const Flashcard = (props) => {
             <h1>{showQuestion ? filteredQuestions[questionIndex] : filteredAnswers[questionIndex]}</h1> 
             <div>{(flashcardCategory !== 'home' && showQuestion) ? <AnswerButton handleAnswer = {onAnswerClick}/> : <NextQuestion handleNext = {onNextQuestion} />
             }</div>
-
+            {showQuestion && <div><button className="delete-button" onClick={()=>{handleDelete(filteredId[questionIndex])}}>DELETE QUESTION</button></div>}
+            {deleted && <p>The question was deleted.</p>}
 
         </div>
      );
