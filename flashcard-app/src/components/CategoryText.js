4f4806e3eb24c67d428da8489a5d6e32f237fba3
MateoGrifsha <mateogrifsha1@gmail.com>
2024-01-16 13:35:54 +0100

    changes

---
+++ flashcard-app/src/components/CategoryText.js
@@ -0,0 +1,7 @@
+const CategoryText = (category) =>{
+    return(
+        <p className="category-label">{'Category: ' + category.category}</p>
+    )
+}
+
+export default CategoryText
--- flashcard-app/src/components/Flashcard.js
+++ flashcard-app/src/components/Flashcard.js
@@ -3,12 +3,14 @@ import useFetch from "../hooks/useFetch";
 import AnswerButton from "./AnswerButton";
 import NextQuestion from "./NextQuestion";
 import { useNavigate } from 'react-router-dom'
+import CategoryText from "./CategoryText";
 
 const Flashcard = (props) => {
     const { data : flashcard, error, isPending} = useFetch(`http://localhost:8000/cards/`);
     const [filteredQuestions, setFilteredQuestions] = useState(Array().fill(null))
     const [filteredAnswers, setfilteredAnswers] = useState(Array().fill(null))
     const [filteredId, setFilteredId] = useState(Array().fill(null))
+    const [filteredFlashcard, setFilteredFlashcard] = useState(Array().fill(null))
     const [showQuestion, setShowQuestion] = useState(true)
     const [questionIndex, setQuestionIndex] = useState(0)
     const [deleted, setDeleted] = useState(false)
@@ -18,6 +20,7 @@ const Flashcard = (props) => {
     if(flashcard){
         flashcard.filter((question) => {
             if(question.category === flashcardCategory){
+                filteredFlashcard.push(question)
                 filteredQuestions.push(question.question)
                 filteredAnswers.push(question.answer)
                 filteredId.push(question.id)
@@ -27,12 +30,12 @@ const Flashcard = (props) => {
     const handleDelete = (flashcardId) => {
         fetch('http://localhost:8000/cards/' + flashcardId, {
             method : 'DELETE'
-          }).then(() => {
+        }).then(() => {
             setDeleted(true)
             setTimeout(()=>{
                 navigate(0)
             }, 1000)
-          })
+        })
     }
     
     const onAnswerClick = () =>{
@@ -41,7 +44,7 @@ const Flashcard = (props) => {
 
     const onNextQuestion = () => {
         setShowQuestion(!showQuestion)
-        setQuestionIndex(Math.floor(Math.random() * filteredQuestions.length))
+        setQuestionIndex(Math.floor(Math.random() * filteredFlashcard.length))
         
     }
 
@@ -49,13 +52,14 @@ const Flashcard = (props) => {
         <div className="flashcard-container">
             {isPending && <div>Loading...</div>}
             {error && <div>{error}</div>}
-            <p className="category-label">{'Category: ' + flashcardCategory}</p>
+
+            <CategoryText category={flashcardCategory} />
+            
             <h1>{showQuestion ? filteredQuestions[questionIndex] : filteredAnswers[questionIndex]}</h1> 
             <div>{(flashcardCategory !== 'home' && showQuestion) ? <AnswerButton handleAnswer = {onAnswerClick}/> : <NextQuestion handleNext = {onNextQuestion} />
             }</div>
             {showQuestion && <div><button className="delete-button" onClick={()=>{handleDelete(filteredId[questionIndex])}}>DELETE QUESTION</button></div>}
             {deleted && <div>The question was deleted.</div>}
-
         </div>
      );
 }
